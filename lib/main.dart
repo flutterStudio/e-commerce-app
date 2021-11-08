31a3782645c807c0bca41c7236676bcf9fea3dfa
samir.ahmad.codavia <samir.ahmad.codavia@gmail.com>
2021-11-08 15:14:28 +0200

    EC-25 init app's requirements on startup.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/src/config/app.config.dart';
 import 'package:e_commerce/src/repository/main.repo.dart';
 import 'package:e_commerce/src/repository/product.repo.dart';
 import 'package:flutter/material.dart';
@@ -6,5 +7,7 @@ import 'package:get/get.dart';
 
 void main() async {
   Get.put<MainRepo>(MainRepo(productRepo: ProductRepo()));
+  await AppConfig().init();
+
   runApp(const Application());
 }
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,31 +1,19 @@
+import 'package:e_commerce/src/config/routing/app_paths.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:e_commerce/src/config/app.config.dart';
 import 'package:e_commerce/src/config/routing/app_routes.dart';
 import 'package:e_commerce/src/config/theming/theming.dart';
 import 'package:e_commerce/src/config/translations/translations.dart';
 import 'package:get/route_manager.dart';
 
-class Application extends StatefulWidget {
+class Application extends StatelessWidget {
   const Application({Key? key}) : super(key: key);
-
-  @override
-  State<Application> createState() => _ApplicationState();
-}
-
-class _ApplicationState extends State<Application> {
-  @override
-  void initState() {
-    AppConfig().init();
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
-    AppConfig().init();
     return GetMaterialApp(
       getPages: AppRoutes.pages,
       translations: AppTranslations(),
+      initialRoute: AppPaths.home,
       locale: Get.deviceLocale,
       supportedLocales: AppTranslations().supportedLocales,
       theme: ThemeConfigs.getTheme(themeMode: ThemeMode.light),
