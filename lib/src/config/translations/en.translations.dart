911ad2190fd56d016f10ed40913fae17cfa9bea1
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-11-11 15:04:18 +0200

    EC-26 Add more english translations.

--- lib/src/config/translations/en.translations.dart
+++ lib/src/config/translations/en.translations.dart
@@ -4,4 +4,23 @@ Map<String, String> _english = {
   "appTitle": "Flutter Template",
   "en": "English",
   "ar": "Arabic",
+}
+  ..addAll(_errors)
+  ..addAll(_messages);
+
+Map<String, String> _errors = {
+  "error-400": "Bad Request",
+  "error-401": "Unauthorized",
+  "error-403": "Forbidden",
+  "error-404": "Not Found",
+  "error-408": "Request Timeout",
+  "error-409": "Operation Denied",
+  "error-500": "Server Error",
+  "error-network": "",
+};
+
+Map<String, String> _messages = {
+  "message-error": "An Error occured",
+  "message-empty-response": "No data found",
+  "message-no-products": "No products found",
 };
