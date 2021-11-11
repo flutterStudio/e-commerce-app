a1c401461d5f79b708f2b72fc993cd2f54c40e79
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-11-11 13:16:08 +0200

    EC-26 add custom exceptions.

--- lib/src/utils/exceptions.utils.dart
+++ lib/src/utils/exceptions.utils.dart
@@ -1,11 +1,41 @@
-class CustomException implements Exception {
-  final String message;
+import 'dart:developer';
 
-  CustomException(this.message);
+class NetworkException implements Exception {
+  final String _message;
+
+  /// Describes a network error with a [message] for the user and a [logMessage]
+  /// for the developers.
+  NetworkException({required String message, String? logMessage})
+      : _message = message {
+    log(message);
+  }
+
+  String get info => "Network Error, $_message";
+  String get message => _message;
 }
 
-class IOException implements Exception {
-  final String message;
+class DatabaseException implements Exception {
+  final String _message;
 
-  IOException(this.message);
+  /// Describes a database error with a [message]for the user and a [logMessage]
+  /// for the developers..
+  DatabaseException({required String message, String? logMessage})
+      : _message = message {
+    log(logMessage ?? message);
+  }
+  String get info => "Database Exception, $_message";
+  String get message => _message;
+}
+
+class DataFormat implements Exception {
+  final String _message;
+
+  /// Describes a formatting error with a [message]for the user and a
+  /// [logMessage] for the developers..
+  DataFormat({required String message, String? logMessage})
+      : _message = message {
+    log(logMessage ?? message);
+  }
+  String get info => "Data Format Exception, $_message";
+  String get message => _message;
 }
