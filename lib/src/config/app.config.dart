bbd3c9f5ff38cb60b20c831269d56bffe69d9a74
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-11-14 13:48:09 +0200

    EC-26 Inject ProductListcontroller in startup .

--- lib/src/config/app.config.dart
+++ lib/src/config/app.config.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce/src/controller/cart_screen.controller.dart';
 import 'package:e_commerce/src/controller/home_screen.controller.dart';
+import 'package:e_commerce/src/controller/product_list.controller.dart';
 import 'package:get/get.dart';
 
 class AppConfig {
@@ -14,5 +15,6 @@ class AppConfig {
   Future<void> _initControllers() async {
     Get.put<HomeScreenController>(HomeScreenController());
     Get.put<CartScreenController>(CartScreenController());
+    Get.put<ProductListcontroller>(ProductListcontroller());
   }
 }
