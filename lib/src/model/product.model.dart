bee8c0864fe06b12bd4113d1743cb2f9fccee6b0
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-11-14 22:17:42 +0200

    EX-26 parse product image info.

--- lib/src/model/product.model.dart
+++ lib/src/model/product.model.dart
@@ -5,7 +5,7 @@ import 'model.dart';
 
 class Product implements Model {
   int? id;
-  String? title, description;
+  String? title, description, mainImage;
   List<String>? images;
   List<Color>? colors;
   double? rating, price;
@@ -39,7 +39,18 @@ class _ProductSerializer extends ModelSerializer<Product> {
     model.images = json["images"];
     model.colors = json["colors"];
     model.price = json["price"];
+    model.mainImage = json["mainImage"]["downloadUrl"];
+    _initImagesList(json);
     return model;
+  }
+
+  void _initImagesList(Map<String, dynamic> json) {
+    var attachments = json["productImagesList"] as List;
+    List<String> images = [];
+    for (var attachment in attachments) {
+      images.add(attachment["attachment"]["downloadUrl"]);
+    }
+    model.images = images;
   }
 
   @override
