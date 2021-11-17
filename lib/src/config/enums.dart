84172353f957dce2b7100d37d3c3d74d31aa4855
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-11-17 14:14:19 +0200

    EC-32 Add data types enums.

---
+++ lib/src/config/enums.dart
@@ -0,0 +1,4 @@
+// ignore: constant_identifier_names
+enum ScreenItemtype { Product, ProductGroup, Image, ImageGroup, Banner }
+// ignore: constant_identifier_names
+enum ScreenItemActionType { External, Internal }
