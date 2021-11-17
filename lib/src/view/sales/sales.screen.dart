2349ca0b400e44e7f9022c5212392c0bf26809f3
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-11-17 15:08:54 +0200

    EC-32 Fetch screen elements.

--- lib/src/view/sales/sales.screen.dart
+++ lib/src/view/sales/sales.screen.dart
@@ -1,61 +1,56 @@
+import 'package:e_commerce/src/config/enums.dart';
+import 'package:e_commerce/src/controller/main_screen.controller.dart';
+import 'package:e_commerce/src/model/main_screen_item.model.dart';
+import 'package:e_commerce/src/view/request_handler.dart';
+import 'package:e_commerce/src/view/sales/components/image_element.component.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
-class SalesScreen extends StatelessWidget {
+class SalesScreen extends GetView<OfferScreenCopntroller> {
   const SalesScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) => _salesScreenContentBuilder(context);
+
+  Widget _salesScreenContentBuilder(BuildContext context) {
+    return Center(
+      child: Obx(() {
+        return RequestHandler<List<ScreenItem>>(
+          data: controller.screenItems.value,
+          onSuccess: (BuildContext context, List<ScreenItem> items) =>
+              _itemsBuilder(
+            context,
+            items,
+          ),
+        );
+      }),
+    );
+  }
+
+  Widget _itemsBuilder(BuildContext context, List<ScreenItem> items) {
     return Column(
       children: [
         Expanded(
           child: SingleChildScrollView(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                GestureDetector(
-                    onTap: () {
-                      Get.toNamed("/products/3");
-                    },
-                    child: Image.asset("assets/images/sale_gaming.png")),
-                const SizedBox(
-                  height: 30,
-                ),
-                GestureDetector(
-                    onTap: () {
-                      Get.toNamed("/products/1");
-                    },
-                    child: Image.asset("assets/images/sale_purse.png")),
-                const SizedBox(
-                  height: 30,
-                ),
-                GestureDetector(
-                    onTap: () {
-                      Get.toNamed("/products/2");
-                    },
-                    child: Image.asset("assets/images/sale_backbag.png")),
-                const SizedBox(
-                  height: 30,
-                ),
-                // SizedBox(
-                //   height: 200,
-                //   child: ListView.builder(
-                //       scrollDirection: Axis.horizontal,
-                //       itemExtent: 100,
-                //       itemCount:
-                //           Get.find<MainRepo>().productRepo.demoProducts.length,
-                //       itemBuilder: (context, index) => GestureDetector(
-                //             onTap: () {
-                //               Get.toNamed(
-                //                   '/product/${Get.find<MainRepo>().productRepo.demoProducts[index].id}');
-                //             },
-                //             child: ProductCard(
-                //                 product: Get.find<MainRepo>()
-                //                     .productRepo
-                //                     .demoProducts[index]),
-                //           )),
-                // )
-              ],
+              children: items.map((e) {
+                switch (e.itemType) {
+                  case ScreenItemtype.Banner:
+                    {
+                      return ImageItem(item: e);
+                    }
+                  case ScreenItemtype.Image:
+                    {
+                      return ImageItem(item: e);
+                    }
+                  //  case ScreenItemtype.ImageGroup:{
+                  //   return ImagesSlider (items: e.);
+                  // }
+                  default:
+                    return Container();
+                }
+              }).toList(),
             ),
           ),
         ),
