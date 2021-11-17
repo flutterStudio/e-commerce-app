21b40b0987bb9a1fbe73a68279f5c98121334452
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-11-17 14:14:45 +0200

    EC-32 Inject OfferRepo in main repo.

--- lib/src/repository/main.repo.dart
+++ lib/src/repository/main.repo.dart
@@ -1,8 +1,11 @@
+import 'package:e_commerce/src/repository/offer.repo.dart';
 import 'package:e_commerce/src/repository/product.repo.dart';
 import 'package:e_commerce/src/service/api.service.dart';
 
 class MainRepo {
   final ProductRepo productRepo;
+  final OfferRepo offerRepo;
   MainRepo({required ApiService apiService})
-      : productRepo = ProductRepo(apiService: apiService);
+      : productRepo = ProductRepo(apiService: apiService),
+        offerRepo = OfferRepo(apiService: apiService);
 }
