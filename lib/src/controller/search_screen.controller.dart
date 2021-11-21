bd1561957edecc94be67ce075f20371133e64a70
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-11-21 13:10:58 +0200

    EC-34 Add search controller

---
+++ lib/src/controller/search_screen.controller.dart
@@ -0,0 +1,7 @@
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get/state_manager.dart';
+import 'package:get/get.dart';
+
+class SearchController extends GetxController {
+  SearchController();
+}
