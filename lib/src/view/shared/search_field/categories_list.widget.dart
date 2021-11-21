904aef5e3142f7575dcb74d3b5122a9049e78412
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-11-21 13:23:03 +0200

    EC-34 Extract CaregoryCard widget to a new file.

--- lib/src/view/shared/search_field/categories_list.widget.dart
+++ lib/src/view/shared/search_field/categories_list.widget.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/src/view/shared/search_field/category_card.widget.dart';
 import 'package:flutter/material.dart';
 
 class Categories extends StatelessWidget {
@@ -22,51 +23,6 @@ class Categories extends StatelessWidget {
             text: categories[index]["text"],
             press: () {},
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class CategoryCard extends StatelessWidget {
-  final bool? isActive;
-  const CategoryCard({
-    Key? key,
-    required this.icon,
-    required this.text,
-    required this.press,
-    this.isActive = false,
-  }) : super(key: key);
-
-  final String? icon, text;
-  final GestureTapCallback press;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: press,
-      child: SizedBox(
-        child: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(10),
-              margin: const EdgeInsets.symmetric(horizontal: 10),
-              decoration: BoxDecoration(
-                color: isActive!
-                    ? Theme.of(context).colorScheme.primaryVariant
-                    : Theme.of(context).colorScheme.secondaryVariant,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Text(
-                text!,
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.subtitle1?.copyWith(
-                    color: isActive!
-                        ? Theme.of(context).colorScheme.primary
-                        : Theme.of(context).colorScheme.secondary),
-              ),
-            ),
-          ],
         ),
       ),
     );
---
+++ lib/src/view/shared/search_field/category_card.widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CategoryCard extends StatelessWidget {
+  final bool? isActive;
+  const CategoryCard({
+    Key? key,
+    required this.icon,
+    required this.text,
+    required this.press,
+    this.isActive = false,
+  }) : super(key: key);
+
+  final String? icon, text;
+  final GestureTapCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: SizedBox(
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(10),
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                color: isActive!
+                    ? Theme.of(context).colorScheme.primaryVariant
+                    : Theme.of(context).colorScheme.secondaryVariant,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Text(
+                text!,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.subtitle1?.copyWith(
+                    color: isActive!
+                        ? Theme.of(context).colorScheme.primary
+                        : Theme.of(context).colorScheme.secondary),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
