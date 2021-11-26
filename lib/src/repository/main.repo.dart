0b538f1cac25280be25b56b72ed92b6b2633eef6
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-11-26 18:19:57 +0200

    EC-31 implement login logic

--- lib/src/repository/main.repo.dart
+++ lib/src/repository/main.repo.dart
@@ -1,8 +1,11 @@
 import 'package:e_commerce/src/repository/product.repo.dart';
+import 'package:e_commerce/src/repository/user.repo.dart';
 import 'package:e_commerce/src/service/api.service.dart';
 
 class MainRepo {
   final ProductRepo productRepo;
+  final UsersRepo usersRepo;
   MainRepo({required ApiService apiService})
-      : productRepo = ProductRepo(apiService: apiService);
+      : productRepo = ProductRepo(apiService: apiService),
+        usersRepo = UsersRepo(apiService: apiService);
 }
