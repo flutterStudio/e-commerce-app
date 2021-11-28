8ac631b760d158c44f5eb33d7f9e1c434ec71410
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-11-28 14:58:54 +0200

    EC-41 Add Size model

---
+++ lib/src/model/size.model.dart
@@ -0,0 +1,30 @@
+import 'package:e_commerce/src/middleware/model_serilizer.middleware.dart';
+import 'package:e_commerce/src/model/model.dart';
+
+class Size extends Model {
+  int? id;
+  String? sizeValue;
+
+  Size({this.id, this.sizeValue});
+
+  @override
+  ModelSerializer<Size> serilizer() {
+    return _SizeSerializer(this);
+  }
+}
+
+class _SizeSerializer extends ModelSerializer<Size> {
+  _SizeSerializer(Size model) : super(model);
+
+  @override
+  Size fromJson(Map<String, dynamic> json) {
+    model.id = json["sizeId"];
+    model.sizeValue = json["sizeValue"];
+    return model;
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    throw UnimplementedError();
+  }
+}
