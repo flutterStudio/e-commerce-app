5d85af52cf90ffc2113b5890fab6e2a8ccea46d1
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-11-29 14:26:55 +0200

    EC-42 Show current user info in the evalution widget.

--- lib/src/view/product_details/components/user_evaluation.widget.dart
+++ lib/src/view/product_details/components/user_evaluation.widget.dart
@@ -1,8 +1,10 @@
 import 'package:e_commerce/src/controller/product.controller.dart';
 import 'package:e_commerce/src/model/evaluation.model.dart';
+import 'package:e_commerce/src/service/auth_service.dart';
 import 'package:e_commerce/src/view/product_details/components/review_item.widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get_state_manager/src/simple/get_view.dart';
+import 'package:get/instance_manager.dart';
 
 class UserEvaluation extends GetView<ProductController> {
   const UserEvaluation({Key? key}) : super(key: key);
@@ -13,6 +15,7 @@ class UserEvaluation extends GetView<ProductController> {
       children: [
         ReviewItem(
             review: Evaluation(
+                user: Get.find<AuthService>().currentUser.value,
                 rate: 4.5,
                 comment:
                     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam a sapien, amet, consectetur nisl ullamcorper quam aliquet. Id nisi, amet suscipit nam orci tincidunt nunc donec arcu. Gravida id ..."))
