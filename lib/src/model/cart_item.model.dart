c9556df131a5ee144fab5df4f1232429fa165baa
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-11-30 20:58:16 +0200

    EC-41 Add color and size properties.

--- lib/src/model/cart_item.model.dart
+++ lib/src/model/cart_item.model.dart
@@ -1,13 +1,17 @@
 import 'package:e_commerce/src/middleware/model_serilizer.middleware.dart';
+import 'package:e_commerce/src/model/color.model.dart';
 import 'package:e_commerce/src/model/model.dart';
 import 'package:e_commerce/src/model/product.model.dart';
+import 'package:e_commerce/src/model/size.model.dart';
 
 class CartItem implements Model {
   Product? product;
   int? orderProductId;
   int? count;
+  ColorModel? color;
+  Size? size;
   double? orderPrice;
-
+  CartItem({this.color, this.count, this.size});
   @override
   ModelSerializer<CartItem> serilizer() {
     return _CartSerializer(this);
