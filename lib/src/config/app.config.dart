1834db145fa548f64b04c80367a1dc6acdffbf99
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-12-05 01:27:54 +0200

    EC-52 Inject  Stories Controller.

--- lib/src/config/app.config.dart
+++ lib/src/config/app.config.dart
@@ -4,6 +4,7 @@ import 'package:e_commerce/src/controller/home_screen.controller.dart';
 import 'package:e_commerce/src/controller/main_screen.controller.dart';
 import 'package:e_commerce/src/controller/product_list.controller.dart';
 import 'package:e_commerce/src/controller/profile.controller.dart';
+import 'package:e_commerce/src/controller/stories.controller.dart';
 import 'package:e_commerce/src/repository/main.repo.dart';
 import 'package:e_commerce/src/service/api.service.dart';
 import 'package:e_commerce/src/service/auth_service.dart';
@@ -24,13 +25,21 @@ class AppConfig {
   Future<void> _initControllers() async {
     await Get.putAsync<HomeScreenController>(
         () async => HomeScreenController());
+
     await Get.putAsync<CartScreenController>(
         () async => CartScreenController());
+
     await Get.putAsync<Discovercontroller>(() async => Discovercontroller());
+
     await Get.putAsync<ProductListcontroller>(
         () async => ProductListcontroller());
+
     await Get.putAsync<OfferScreenCopntroller>(
         () async => OfferScreenCopntroller());
+
+    await Get.putAsync<StoriesController>(() async => StoriesController())
+        .then((value) => value.getAllCompaniesStories());
+
     await Get.putAsync<ProfileController>(() async => ProfileController());
   }
 
