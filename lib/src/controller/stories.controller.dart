69d7be5248c39a6f5cb5819be3a4b0e729cbf9ce
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-12-04 22:50:18 +0200

    EC-52 Implement the functionality to fetch stories through repo layer..

--- lib/src/controller/stories.controller.dart
+++ lib/src/controller/stories.controller.dart
@@ -1,4 +1,39 @@
+import 'package:e_commerce/src/model/data.model.dart';
+import 'package:e_commerce/src/model/story.model.dart';
+import 'package:e_commerce/src/repository/main.repo.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 
-class StoriesController extends GetxController {}
+class StoriesController extends GetxController {
+  Rx<Data<List<Story>>> allCompaniesStories =
+      Rx<Data<List<Story>>>(Data.empty());
+
+  Rx<Data<List<Story>>> currentCompanySroies =
+      Rx<Data<List<Story>>>(Data.empty());
+
+  Rx<int?> currentMarketId = Rx<int?>(null);
+
+  final MainRepo _mainRepo = Get.find<MainRepo>();
+
+  Future<void> getAllCompaniesStories() async {
+    allCompaniesStories.value = Data.inProgress();
+    allCompaniesStories.value =
+        await _mainRepo.storiesRepo.getAllMarketsStories();
+  }
+
+  Future<void> getCurrentCompanyStories() async {
+    if (currentMarketId.value == null) {
+      return;
+    }
+    currentCompanySroies.value = Data.inProgress();
+    currentCompanySroies.value =
+        await _mainRepo.storiesRepo.getCompanyStories(currentMarketId.value!);
+  }
+
+  void setCurrentCompany(int id) {
+    if (currentMarketId.value != id) {
+      currentMarketId.value = id;
+      getCurrentCompanyStories();
+    }
+  }
+}
