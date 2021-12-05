c0ced870cbb9bb55573696dc86d18606a5c0c267
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-12-05 22:37:36 +0200

    EC-51 Add errors handling to exceptions..

--- lib/src/utils/exceptions.utils.dart
+++ lib/src/utils/exceptions.utils.dart
@@ -3,52 +3,61 @@ import 'dart:developer';
 import 'package:e_commerce/src/config/enums.dart';
 
 abstract class BaseException implements Exception {
-  BaseException({this.errorType = ErrorType.none});
+  BaseException({this.errorType = ErrorType.none, required this.message});
   final ErrorType? errorType;
+  String message;
+
+  void handleEception() {
+    switch (errorType) {
+      case ErrorType.forbidden:
+        {
+          message = "Access denied";
+          break;
+        }
+      case ErrorType.unauthorized:
+        {
+          break;
+        }
+      default:
+    }
+  }
 }
 
 class NetworkException extends BaseException {
-  final String _message;
-
   /// Describes a network error with a [message] for the user and a [logMessage]
   /// for the developers.
   NetworkException(
       {required String message, String? logMessage, ErrorType? errorType})
-      : _message = message,
-        super(errorType: errorType) {
+      : super(errorType: errorType, message: message) {
     log(message);
   }
 
-  String get info => "Network Error, $_message";
-  String get message => _message;
+  @override
+  String get message => "Network Error, $message";
 }
 
 class DatabaseException extends BaseException {
-  final String _message;
-
   /// Describes a database error with a [message]for the user and a [logMessage]
   /// for the developers..
   DatabaseException(
       {required String message, String? logMessage, ErrorType? errorType})
-      : _message = message,
-        super(errorType: errorType) {
+      : super(errorType: errorType, message: message) {
     log(logMessage ?? message);
   }
-  String get info => "Database Exception, $_message";
-  String get message => _message;
+
+  @override
+  String get message => "Database Error, $message";
 }
 
-class DataFormat extends BaseException {
-  final String _message;
-
+class DataFormatException extends BaseException {
   /// Describes a formatting error with a [message]for the user and a
   /// [logMessage] for the developers..
-  DataFormat(
+  DataFormatException(
       {required String message, String? logMessage, ErrorType? errorType})
-      : _message = message,
-        super(errorType: errorType) {
+      : super(errorType: errorType, message: message) {
     log(logMessage ?? message);
   }
-  String get info => "Data Format Exception, $_message";
-  String get message => _message;
+
+  @override
+  String get message => "DataFormat Error, $message";
 }
