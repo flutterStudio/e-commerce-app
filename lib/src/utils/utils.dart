4f6140c9edb7e66d051e0f2c3eee4b0d7d542530
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-12-05 20:37:21 +0200

    EC-51 Add showSnackBar util function .

--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -1,6 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
 class Utils {
   static T? enumFromString<T>(Iterable<T> values, String value) =>
       values.firstWhere(
         (type) => type.toString().split(".").last == value,
       );
+
+  static void showSnackBar(String message,
+      {Color? color,
+      Color? background,
+      Widget? button,
+      bool showProgressIndicator = false}) {
+    Get.showSnackbar(
+      GetSnackBar(
+        snackStyle: SnackStyle.FLOATING,
+        showProgressIndicator: showProgressIndicator,
+        barBlur: 0.5,
+        mainButton: button,
+        messageText: Text(
+          message,
+          style: Get.textTheme.subtitle2
+              ?.copyWith(color: color ?? Get.theme.colorScheme.onError),
+        ),
+        backgroundColor: background ?? Get.theme.colorScheme.error,
+      ),
+    );
+  }
 }
