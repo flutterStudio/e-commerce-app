1647fb8dff02308c34552c76c486d4fa61bfa14d
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-12-05 14:05:19 +0200

    EC-52 Figure out story file type.

--- lib/src/model/story.model.dart
+++ lib/src/model/story.model.dart
@@ -1,11 +1,13 @@
+import 'package:e_commerce/src/config/enums.dart';
 import 'package:e_commerce/src/middleware/model_serilizer.middleware.dart';
 import 'package:e_commerce/src/model/model.dart';
+import 'package:e_commerce/src/utils/file.utils.dart';
 
 class Story extends Model {
   int? id;
   String? attachment;
   int? userId;
-
+  MediaType? mediaType;
   Story({this.id, this.attachment});
 
   @override
@@ -21,14 +23,22 @@ class _StorySerializer extends ModelSerializer<Story> {
   Story fromJson(Map<String, dynamic> json) {
     model.id = json.containsKey('StoryId') ? json["StoryId"] : null;
     model.attachment = json.containsKey('attachment')
-        ? json['attachment']["downloadUrl"]
+        ? _emulatorImage(json['attachment']["downloadUrl"])
         : null;
     model.userId = json.containsKey('user') ? json['user']["userId"] : null;
+    model.mediaType = model.attachment != null
+        ? FileUtils.filesTypes[json['attachment']['type']]
+        : null;
     return model;
   }
 
   @override
   Map<String, dynamic> toJson() {
     throw UnimplementedError();
+  }
+
+  String? _emulatorImage(String? url) {
+    return url?.replaceFirst(
+        "https://127.0.0.1:5001", "http://94.242.58.41:5002");
   }
 }
