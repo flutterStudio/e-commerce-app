c111034781a708e1e41898e96ed6bee2692d756a
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-12-05 22:10:29 +0200

    EC-51 Add ErrorType to custom exceptions.

--- lib/src/utils/exceptions.utils.dart
+++ lib/src/utils/exceptions.utils.dart
@@ -1,12 +1,21 @@
 import 'dart:developer';
 
-class NetworkException implements Exception {
+import 'package:e_commerce/src/config/enums.dart';
+
+abstract class BaseException implements Exception {
+  BaseException({this.errorType = ErrorType.none});
+  final ErrorType? errorType;
+}
+
+class NetworkException extends BaseException {
   final String _message;
 
   /// Describes a network error with a [message] for the user and a [logMessage]
   /// for the developers.
-  NetworkException({required String message, String? logMessage})
-      : _message = message {
+  NetworkException(
+      {required String message, String? logMessage, ErrorType? errorType})
+      : _message = message,
+        super(errorType: errorType) {
     log(message);
   }
 
@@ -14,26 +23,30 @@ class NetworkException implements Exception {
   String get message => _message;
 }
 
-class DatabaseException implements Exception {
+class DatabaseException extends BaseException {
   final String _message;
 
   /// Describes a database error with a [message]for the user and a [logMessage]
   /// for the developers..
-  DatabaseException({required String message, String? logMessage})
-      : _message = message {
+  DatabaseException(
+      {required String message, String? logMessage, ErrorType? errorType})
+      : _message = message,
+        super(errorType: errorType) {
     log(logMessage ?? message);
   }
   String get info => "Database Exception, $_message";
   String get message => _message;
 }
 
-class DataFormat implements Exception {
+class DataFormat extends BaseException {
   final String _message;
 
   /// Describes a formatting error with a [message]for the user and a
   /// [logMessage] for the developers..
-  DataFormat({required String message, String? logMessage})
-      : _message = message {
+  DataFormat(
+      {required String message, String? logMessage, ErrorType? errorType})
+      : _message = message,
+        super(errorType: errorType) {
     log(logMessage ?? message);
   }
   String get info => "Data Format Exception, $_message";
