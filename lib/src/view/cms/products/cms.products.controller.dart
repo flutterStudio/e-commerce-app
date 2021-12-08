e01627e339de5cab90c7a7ec61e044fd5434f322
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-12-08 17:03:16 +0200

    EC-54 Add cms prefix to files names.

---
+++ lib/src/view/cms/products/cms.products.controller.dart
@@ -0,0 +1,22 @@
+import 'package:e_commerce/src/model/data.model.dart';
+import 'package:e_commerce/src/model/product.model.dart';
+import 'package:e_commerce/src/repository/main.repo.dart';
+import 'package:get/get_rx/get_rx.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:get/instance_manager.dart';
+
+class CMSPRoductsController extends GetxController {
+  int? type;
+  Rx<int?> offer = Rx<int?>(null);
+  Rx<Data<List<Product>>> products = Rx<Data<List<Product>>>(Data.empty());
+
+  CMSPRoductsController({this.type});
+
+  Future<void> getProducts() async {
+    products.value = Data.inProgress();
+    Data<List<Product>> companyProducts =
+        await Get.find<MainRepo>().productRepo.getActiveProducts();
+
+    products.value = companyProducts;
+  }
+}
