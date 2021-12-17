c6bf12a5b7dc38ca6429b288b55d7fa1bc6152bb
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-12-17 22:04:16 +0200

    EC-54 Make stories screen refreshable.

--- lib/src/view/cms/stories/cms.stories.screen.dart
+++ lib/src/view/cms/stories/cms.stories.screen.dart
@@ -1,17 +1,22 @@
 import 'package:e_commerce/src/view/cms/stories/cms.stories.controller.dart';
 import 'package:e_commerce/src/view/cms/stories/components/stories_grid.widget.dart';
+import 'package:e_commerce/src/view/mixinx/refreshable.mixin.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/simple/get_view.dart';
 
-class CMSStoriesScreen extends GetView<CMSStoriesController> {
+class CMSStoriesScreen extends GetView<CMSStoriesController>
+    with RefreshableMixin {
   const CMSStoriesScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: StoriesGrid(),
-    );
+  Widget body(BuildContext context) {
+    return const StoriesGrid();
+  }
+
+  @override
+  Future<void> onRefresh() {
+    return controller.getStories();
   }
 }
