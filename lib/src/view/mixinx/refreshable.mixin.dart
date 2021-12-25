09466509ec1cc86b524e28dea98c02377699d9c6
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-12-25 22:20:31 +0200

    EC-54  use Smart refresher to implement load  more.

--- lib/src/view/mixinx/refreshable.mixin.dart
+++ lib/src/view/mixinx/refreshable.mixin.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 mixin RefreshableMixin on StatelessWidget {
   Widget body(BuildContext context);
+  Widget? headerBuiler(BuildContext context,
+          {Widget? title, Widget? leading}) =>
+      title == null && leading == null
+          ? null
+          : SliverAppBar(
+              leading: leading ?? const BackButton(),
+              title: title,
+            );
   Future<void> onRefresh();
-  VoidCallback? onLoadMore() => null;
-
+  VoidCallback? onloadMore() => null;
+  RefreshController refreshController() => RefreshController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: RefreshIndicator(child: body(context), onRefresh: onRefresh));
+        body: SmartRefresher(
+      enablePullDown: true,
+      enablePullUp: true,
+      controller: refreshController(),
+      onRefresh: () {
+        onRefresh();
+      },
+      onLoading: () {
+        onloadMore();
+      },
+      child: body(context),
+    ));
   }
 }
