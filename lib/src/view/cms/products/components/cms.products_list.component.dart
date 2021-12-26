aa62c7e92ab4f378962b19428a794d2e44040637
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-12-27 00:14:33 +0200

    EC-54  Fix products pagination.

--- lib/src/view/cms/products/components/cms.products_list.component.dart
+++ lib/src/view/cms/products/components/cms.products_list.component.dart
@@ -3,6 +3,7 @@ import 'package:e_commerce/src/view/cms/products/cms.products.controller.dart';
 import 'package:e_commerce/src/view/shared/custom_network_image.widget.dart';
 import 'package:e_commerce/src/view/shared/request_handler.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
@@ -16,56 +17,59 @@ class CMSProductsList extends GetWidget<CMSProductsController> {
     return Obx(() {
       return RequestHandler<List<Product>>(
         data: controller.products.value,
-        onSuccess: (BuildContext context, List<Product> items) =>
-            ListView.builder(
-                shrinkWrap: true,
-                itemCount: controller.products.value.data!.length,
-                itemBuilder: (context, index) {
-                  Product product = items[index];
-                  return ExpansionTile(
-                    children: [
-                      Row(children: [
-                        IconButton(
-                            onPressed: product.id == null
-                                ? null
-                                : () {
-                                    controller.deleteProduct(product.id!);
-                                  },
-                            icon: const Icon(Icons.delete)),
-                        IconButton(
-                            onPressed: () {}, icon: const Icon(Icons.edit)),
-                      ])
-                    ],
-                    leading: CustomNetworkImage(
-                      url: product.mainImage ?? "",
+        onSuccess: (BuildContext context, List<Product> items) => Container(
+          margin: const EdgeInsets.symmetric(vertical: 20),
+          child: ListView.builder(
+              // shrinkWrap: true,
+              dragStartBehavior: DragStartBehavior.start,
+              physics: const ClampingScrollPhysics(),
+              itemCount: controller.products.value.data!.length,
+              itemBuilder: (context, index) {
+                Product product = items[index];
+                return ExpansionTile(
+                  children: [
+                    Row(children: [
+                      IconButton(
+                          onPressed: product.id == null
+                              ? null
+                              : () {
+                                  controller.deleteProduct(product.id!);
+                                },
+                          icon: const Icon(Icons.delete)),
+                      IconButton(
+                          onPressed: () {}, icon: const Icon(Icons.edit)),
+                    ])
+                  ],
+                  leading: CustomNetworkImage(
+                    url: product.mainImage ?? "",
+                  ),
+                  title: Text(
+                    product.title ?? "",
+                    style: const TextStyle(color: Colors.black, fontSize: 16),
+                    overflow: TextOverflow.clip,
+                    maxLines: 2,
+                  ),
+                  subtitle: Text.rich(
+                    TextSpan(
+                      text: "\$${product.price}",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          color: Theme.of(context).colorScheme.primary),
+                      children: [
+                        product.discount != null
+                            ? TextSpan(
+                                text: " disount: ${product.discount}",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    color:
+                                        Theme.of(context).colorScheme.primary))
+                            : const TextSpan(),
+                      ],
                     ),
-                    title: Text(
-                      product.title ?? "",
-                      style: const TextStyle(color: Colors.black, fontSize: 16),
-                      overflow: TextOverflow.clip,
-                      maxLines: 2,
-                    ),
-                    subtitle: Text.rich(
-                      TextSpan(
-                        text: "\$${product.price}",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w600,
-                            color: Theme.of(context).colorScheme.primary),
-                        children: [
-                          product.discount != null
-                              ? TextSpan(
-                                  text: " disount: ${product.discount}",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w600,
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .primary))
-                              : const TextSpan(),
-                        ],
-                      ),
-                    ),
-                  );
-                }),
+                  ),
+                );
+              }),
+        ),
       );
     });
   }
