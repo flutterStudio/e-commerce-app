02ebed4ead421f0b41ccf24cf4ca94800766d18d
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-12-27 15:02:48 +0200

    EC-54  Clear contents on close.

--- lib/src/view/cms/add_product/cms.add_product.screen.dart
+++ lib/src/view/cms/add_product/cms.add_product.screen.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/src/view/cms/add_product/cms.add_product.controller.dart';
 import 'package:e_commerce/src/view/cms/add_product/components/add_product_form.dart';
 import 'package:flutter/material.dart';
 
@@ -7,19 +8,24 @@ class CMSAddProduct extends StatelessWidget {
   const CMSAddProduct({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("add-product".tr),
-      ),
-      body: const SizedBox(
-        width: double.infinity,
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          child: SingleChildScrollView(
-            child: AddProductForm(),
+    return WillPopScope(
+        onWillPop: () async {
+          Get.find<CMSAddProductController>().clearAll();
+          return true;
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("add-product".tr),
           ),
-        ),
-      ),
-    );
+          body: const SizedBox(
+            width: double.infinity,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: SingleChildScrollView(
+                child: AddProductForm(),
+              ),
+            ),
+          ),
+        ));
   }
 }
